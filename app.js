3bdd060a995a092301657b344f52eca9008f0408
Simon Jung <drinkwater0502@gmail.com>
2021-03-11 18:48:09 -0800

    added JS file

---
+++ app.js
@@ -0,0 +1,9 @@
+let numPets = 1
+let namePets = 'Pokey'
+const numKeysOnPiano = 88
+const newboolean = Boolean(true)
+if (numPets === numKeysOnPiano) {
+    console.log('They match')
+  } else {
+    console.log('No luck')
+  }
